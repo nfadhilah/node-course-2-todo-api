91f7b221f5e1636dd52f809a8a861676714a2884
Mono Noor Fadhilah <halilintar@gmail.com>
2017-11-24 11:48:11 +0800

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -39,6 +39,23 @@ UserSchema.methods.toJSON = function () {
   return _.pick(userObject, ['_id', 'email']);
 };
 
+UserSchema.statics.findByToken = function(token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    _id: decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
+  })
+};
+
 UserSchema.methods.generateAuthToken = function() {
   var user = this;
   var access = 'auth';
--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var mongoose = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 
 var app = express();
@@ -52,7 +53,6 @@ app.get('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 });
-
 
 app.delete('/todos/:id', (req, res) => {
   var id = req.params.id;
@@ -108,6 +108,10 @@ app.post('/users', (req, res) => {
   }).catch((err) => {
     res.send(400).send(err);
   });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);
 });
 
 app.listen(port, () => {
